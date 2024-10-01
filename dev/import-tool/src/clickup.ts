e59eccdc94588784fe128a683820796a714cd841
Anna Khismatullina <anna.khismatullina@gmail.com>
2024-10-02 01:32:43 +0700

    Create Projects
    Signed-off-by: Anna Khismatullina <anna.khismatullina@gmail.com>

--- dev/import-tool/src/clickup.ts
+++ dev/import-tool/src/clickup.ts
@@ -1,70 +1,52 @@
-import {
-  type AnyAttribute,
-  type AttachedData, type Class, ClassifierKind,
-  type CollaborativeDoc,
-  collaborativeDocParse, type Data, type Doc,
-  generateId, Hyperlink,
+import core, {
+  type AttachedData,
+  type Class,
+  collaborativeDocParse,
+  generateId,
   makeCollaborativeDoc,
   type Ref,
-  SortingOrder,
-  type Status,
   type TxOperations
 } from '@hcengineering/core'
 import { type FileUploader } from './fileUploader'
 import document, { type Document, type Teamspace } from '@hcengineering/document'
 import tracker from '@hcengineering/tracker'
-import pluginState, { type Issue, IssuePriority, type Project } from '@hcengineering/tracker'
+import pluginState, {
+  type Issue,
+  IssuePriority,
+  type IssueStatus,
+  type Project,
+  TimeReportDayType
+} from '@hcengineering/tracker'
 import { jsonToYDocNoSchema, parseMessageMarkdown } from '@hcengineering/text'
 import { yDocToBuffer } from '@hcengineering/collaboration'
 import { readdir, readFile } from 'fs/promises'
 import { join, parse } from 'path'
 import csv from 'csvtojson'
-import core from '@hcengineering/model-core'
-import { makeRank } from '@hcengineering/rank'
-import task, { createProjectType, type ProjectType, type TaskType, type Task } from '@hcengineering/task'
-import { getEmbeddedLabel } from '@hcengineering/platform'
+import task, { createProjectType, type ProjectType, type Task, type TaskType } from '@hcengineering/task'
+import { type ImportIssue } from './importer/importer'
 
 type ClickupId = string
 type HulyId = string
 
-interface FileMetadata {
-  fullPath: string
-  extension: string
-}
-
-interface DocumentMetadata {
-  id: HulyId
-  clickupId: ClickupId
-  name: string
-}
-
-type FileMetadataMap = Map<ClickupId, FileMetadata>
-type DocumentMetadataMap = Map<HulyId, DocumentMetadata>
-
 interface ClickupTask {
-  'Task ID': string
+  'Task ID': ClickupId
   'Task Name': string
   'Task Content': string
   'Status': string
   'Parent ID': string
   'Attachments': string[]
   'Assignees': string[]
-  'Comments': string // todo: obj
+  'Priority'?: number
+  'Space Name': string
+  'Checklists': string // todo: obj
+  'Comments': string // todo: obj[]
   'Time Estimated': number
+  'Time Spent': number
 }
-
-interface HulyIssue {
-  'title': string
-  'description': string
-  'status': Ref<Status>
-  'estimation': number
-}
-
-const StatusMap = new Map <string, Ref<Status>>()
-StatusMap.set('выполнено', tracker.status.Done)
 
 const CLICKUP_PROJECT_TYPE_ID = '66f93d911c6b497d54ad5675' as Ref<ProjectType>
 const CLICKUP_TASK_TYPE_ID = '66fbf115093abbaab6dd54b7' as Ref<TaskType>
+const CLICKUP_MIXIN_ID = `${CLICKUP_TASK_TYPE_ID}:type:mixin` as Ref<Class<Task>>
 
 export async function importClickUp (
   client: TxOperations,
@@ -95,7 +77,7 @@ async function processClickupDocument (
   teamspace: string & {
     __ref: Teamspace
   }
-) {
+): Promise<void> {
   const data = await readFile(fullPath)
   await importPageDocument(client, uploadFile, docName, data, teamspace)
 }
@@ -108,31 +90,43 @@ async function processClickupTasks (
   const jsonArray = await csv().fromFile(fullPath)
   console.log(jsonArray)
 
-  const statuses: string[] = []
+  const clickupHulyIdMap = new Map<ClickupId, HulyId>()
+  const statuses = new Set<string>()
+  const projects = new Set<string>()
+  const persons = new Set<string>()
   for (const json of jsonArray) {
     const clickupTask = json as ClickupTask
-
-    statuses.push(clickupTask.Status)
-
     console.log(clickupTask)
-    const issue = convertClickupToHullyIssue(clickupTask)
-    console.log(issue)
+    clickupHulyIdMap.set(clickupTask['Task ID'], generateId())
+    statuses.add(clickupTask.Status)
+    projects.add(clickupTask['Space Name'])
 
-    // await importIssue(client, uploadFile, issue, DEFAULT_PROJECT_FIXME)
-    // console.log(issue)
+    // clickupTask.Assignees.forEach((name) => {
+    //   persons.add(name)
+    // })
   }
+  console.log(clickupHulyIdMap)
   console.log(statuses)
-  console.log(statuses)
+  console.log(projects)
+  console.log(persons)
 
-  await createClickUpProjectType(client, statuses)
+  const projectType = await createClickUpProjectType(client, Array.from(statuses))
+  for (const project of projects) {
+    createProject(client, project, projectType)
+  }
 }
 
-function convertClickupToHullyIssue (task: ClickupTask): HulyIssue {
+function convertToImportIssue (task: ClickupTask): ImportIssue {
+  const estimation = task['Time Estimated']
+  const remainingTime = estimation - task['Time Spent']
   return {
     title: '[' + task['Task ID'] + '] ' + task['Task Name'],
     description: task['Task Content'],
-    status: StatusMap.get(task.Status) ?? tracker.status.Backlog,
-    estimation: task['Time Estimated'] ?? 0
+    assignee: null, // todo
+    status: tracker.status.Todo, // todo
+    priority: IssuePriority.NoPriority, // todo
+    estimation,
+    remainingTime
   }
 }
 
@@ -185,188 +179,15 @@ async function importPageDocument (
   )
 }
 
-async function importIssue (
-  client: TxOperations,
-  uploadFile: FileUploader,
-  data: HulyIssue,
-  space: Ref<Project>
-): Promise<void> {
-  const id: Ref<Issue> = generateId()
-  const lastOne = await client.findOne<Issue>(
-    tracker.class.Issue,
-    { space },
-    { sort: { rank: SortingOrder.Descending } }
-  )
-  const incResult = await client.updateDoc(
-    tracker.class.Project,
-    core.space.Space,
-    space,
-    {
-      $inc: { sequence: 1 }
-    },
-    true
-  )
-
-  const proj = await client.findOne(tracker.class.Project, { _id: space })
-  const number = (incResult as any).object.sequence
-  const identifier = `${proj?.identifier}-${number}`
-
-  const taskKind = proj?.type !== undefined ? { parent: proj.type } : {}
-  const kind = (await client.findOne(task.class.TaskType, taskKind)) as TaskType
-
-  const collabId = await importIssueDescription(uploadFile, id, data.description)
-
-  const taskToCreate = {
-    title: data.title,
-    description: collabId,
-    assignee: null,
-    component: null,
-    number,
-    status: data.status,
-    priority: IssuePriority.NoPriority,
-    rank: makeRank(lastOne?.rank, undefined),
-    comments: 0,
-    subIssues: 0,
-    dueDate: null,
-    parents: [],
-    reportedTime: 0,
-    remainingTime: 0,
-    estimation: data.estimation,
-    reports: 0,
-    childInfo: [],
-    identifier,
-    kind: kind._id
-  }
-  await client.addCollection(
-    tracker.class.Issue,
-    space,
-    tracker.ids.NoParent,
-    tracker.class.Issue,
-    'subIssues',
-    taskToCreate,
-    id
-  )
-}
-
-async function importIssueDescription (
-  uploadFile: FileUploader,
-  id: Ref<Issue>,
-  data: string
-): Promise<CollaborativeDoc> {
-  const json = parseMessageMarkdown(data ?? '', 'image://')
-  const collabId = makeCollaborativeDoc(id, 'description')
-
-  const yDoc = jsonToYDocNoSchema(json, 'description')
-  const { documentId } = collaborativeDocParse(collabId)
-  const buffer = yDocToBuffer(yDoc)
-
-  const form = new FormData()
-  const file = new File([new Blob([buffer])], collabId)
-  form.append('file', file, documentId)
-  form.append('type', 'application/ydoc')
-  form.append('size', buffer.length.toString())
-  form.append('name', collabId)
-  form.append('id', id)
-  form.append('data', new Blob([buffer])) // ?
-
-  await uploadFile(id, form)
-
-  return collabId
-}
-
 export interface ClickupIssue extends Issue {
   clickupId: string
-}
-
-async function createSimpleAttribute (
-  client: TxOperations,
-  defaultValue: string,
-  mixinClass: Ref<Class<Doc>>
-): Promise<void> {
-  const data: Data<AnyAttribute> = {
-    attributeOf: mixinClass,
-    name: 'ClickUp ID',
-    label: 'ClickUp ID',
-    isCustom: true,
-    type: {
-      _class: core.class.TypeString,
-      label: 'ClickUp ID'
-    },
-    defaultValue
-  }
-  // Create new attribute
-  await client.createDoc(
-    core.class.Attribute,
-    core.space.Model,
-    data,
-    undefined
-  )
 }
 
 export async function createClickUpProjectType (
   client: TxOperations,
   statuses: string[]
-): Promise<void> {
-  // Create target class for custom field.
-  // NOTE: it is important for this id to be consistent when re-creating the same
-  // task type with the same id as it will happen during every migration if type is created by the system
-  // const targetClassId = `${CLICKUP_TASK_TYPE_ID}:type:mixin` as Ref<Class<Task>>
-  // const ofClassClass = client.getHierarchy().getClass(tracker.class.Issue)
-  //
-  // const mixinId = await client.createDoc(
-  //   core.class.Mixin,
-  //   core.space.Model,
-  //   {
-  //     extends: tracker.class.Issue, // ClickupIssue
-  //     kind: ClassifierKind.MIXIN,
-  //     label: ofClassClass.label,
-  //     icon: ofClassClass.icon,
-  //     hidden: false
-  //   },
-  //   targetClassId
-  // )
-  //
-  // await client.createMixin(
-  //   targetClassId,
-  //   core.class.Mixin,
-  //   core.space.Model,
-  //   task.mixin.TaskTypeClass, {
-  //     taskType: CLICKUP_TASK_TYPE_ID,
-  //     projectType: CLICKUP_PROJECT_TYPE_ID
-  //   })
-  //
-  // if (!client.getHierarchy().hasMixin(projectInst, github.mixin.GithubProject)) {
-  //   // We need to add GithubProject mixin
-  //   const mixinId = await client.createDoc(core.class.Mixin, core.space.Model, {
-  //     extends: github.mixin.GithubIssue,
-  //     kind: ClassifierKind.MIXIN,
-  //     label: getEmbeddedLabel(projectInst.name),
-  //     hidden: false,
-  //     icon: github.icon.Github
-  //   })
-  //   await client.createMixin(
-  //     projectInst._id,
-  //     tracker.class.Issue,
-  //     core.space.Space,
-  //     github.mixin.GithubProject,
-  //     {
-  //       integration: integration._id,
-  //       repositories: [],
-  //       mixinClass: mixinId,
-  //       mappings: []
-  //     }
-  //   )
-  // }
-
-  // await this.client.createMixin(targetClassId, tracker.class.Issue, prj._id, github.mixin.GithubIssue, {
-  //   githubNumber: issueExternal.number,
-  //   url: issueExternal.url,
-  //   repository: repo,
-  //   descriptionLocked: isDescriptionLocked
-  // })
-  // await this.client.createMixin<Issue, Issue>(issueId, github.mixin.GithubIssue, prj._id, prj.mixinClass, {})
-
-  await createProjectType(client, {
+): Promise<Ref<ProjectType>> {
+  return await createProjectType(client, {
     name: 'ClickUp project',
     descriptor: tracker.descriptors.ProjectType,
     shortDescription: 'For issues imported from ClickUp',
@@ -380,7 +201,6 @@ export async function createClickUpProjectType (
     kind: 'both',
     name: 'ClickUp issue',
     ofClass: tracker.class.Issue,
-    // targetClass: targetClassId,
     statusCategories: [task.statusCategory.UnStarted],
     statusClass: tracker.class.IssueStatus,
     icon: tracker.icon.Issue,
@@ -396,4 +216,41 @@ export async function createClickUpProjectType (
   }],
   CLICKUP_PROJECT_TYPE_ID
   )
+}
+
+async function createProject (
+  client: TxOperations,
+  name: string,
+  typeId: Ref<ProjectType>
+): Promise<Ref<Project>> {
+  const projectId = generateId<Project>()
+  const projectData = {
+    name,
+    description: '',
+    private: false,
+    members: [],
+    owners: [],
+    archived: false,
+    autoJoin: false,
+    identifier: 'CLICK',
+    sequence: 0,
+    defaultAssignee: undefined,
+    defaultIssueStatus: '' as Ref<IssueStatus>,
+    defaultTimeReportDay: TimeReportDayType.PreviousWorkDay
+  }
+  await client.createDoc(
+    tracker.class.Project,
+    core.space.Space,
+    { ...projectData, type: typeId },
+    projectId
+  )
+  // Add space type's mixin with roles assignments
+  await client.createMixin(
+    projectId,
+    tracker.class.Project,
+    core.space.Space,
+    CLICKUP_MIXIN_ID,
+    {}
+  )
+  return projectId
 }
